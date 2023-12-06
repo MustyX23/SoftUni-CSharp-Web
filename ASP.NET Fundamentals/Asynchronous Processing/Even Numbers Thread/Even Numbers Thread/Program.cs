357d753369b3d265724d2d2d2cc4a0cfd217968d
Mustafa Buhov <mustyx900@gmail.com>
2023-12-06 08:04:29 +0200

    Create Program.cs

---
+++ ASP.NET Fundamentals/Asynchronous Processing/Even Numbers Thread/Even Numbers Thread/Program.cs
@@ -0,0 +1,13 @@
+﻿//Problem 1
+
+Task task = Task.Run(() =>
+{
+    Parallel.For(0, 1000, number =>
+    {
+        Console.WriteLine(number);
+    });
+});
+
+await task;
+
+Console.WriteLine("Thread finished work");

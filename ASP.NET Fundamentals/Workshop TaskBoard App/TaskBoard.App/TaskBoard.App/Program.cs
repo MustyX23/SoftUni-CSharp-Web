a9f7ef84ce590010bf0d70103a3e58b2cede77e9
MustyX23 <mustyx900@gmail.com>
2023-12-20 22:18:44 +0200

    Renamed the ApplicationDbContext to TaskBoardDbContext

--- ASP.NET Fundamentals/Workshop TaskBoard App/TaskBoard.App/TaskBoard.App/Program.cs
+++ ASP.NET Fundamentals/Workshop TaskBoard App/TaskBoard.App/TaskBoard.App/Program.cs
@@ -12,7 +12,7 @@ namespace TaskBoard.App
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<TaskBoardDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
 
@@ -27,7 +27,7 @@ namespace TaskBoard.App
                     options.Password.RequireUppercase = false;
                     options.Password.RequireLowercase = false;
                 })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<TaskBoardDbContext>();
 
             builder.Services.AddControllersWithViews();
 

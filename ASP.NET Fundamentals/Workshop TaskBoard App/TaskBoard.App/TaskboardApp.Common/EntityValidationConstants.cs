dd3321bce08228a3a61a53326dc95cbf613c4612
MustyX23 <mustyx900@gmail.com>
2023-12-20 22:18:48 +0200

    Update EntityValidationConstants.cs

--- ASP.NET Fundamentals/Workshop TaskBoard App/TaskBoard.App/TaskboardApp.Common/EntityValidationConstants.cs
+++ ASP.NET Fundamentals/Workshop TaskBoard App/TaskBoard.App/TaskboardApp.Common/EntityValidationConstants.cs
@@ -10,5 +10,10 @@
             public const int TaskMaxDescription = 1000;
             public const int TaskMinDescription = 10;
         }
+        public static class Board
+        {
+            public const int BoardMaxName = 30;
+            public const int BoardMinName = 3;
+        }
     }
 }

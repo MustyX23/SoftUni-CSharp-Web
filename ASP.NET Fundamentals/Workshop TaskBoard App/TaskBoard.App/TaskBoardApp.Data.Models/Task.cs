6f7ab384a45af2423dce686cfcfb15d0289bd308
MustyX23 <mustyx900@gmail.com>
2023-12-20 22:18:54 +0200

    Create Task.cs

---
+++ ASP.NET Fundamentals/Workshop TaskBoard App/TaskBoard.App/TaskBoardApp.Data.Models/Task.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static TaskboardApp.Common.EntityValidationConstants.Task;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Task
+    {
+        public Task()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(TaskMaxTitle)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TaskMaxDescription)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey(nameof(Board))]
+        public int BoardId { get; set; }
+
+        public virtual Board Board { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; } = null!;
+
+        public virtual IdentityUser Owner { get; set; }
+    }
+}
